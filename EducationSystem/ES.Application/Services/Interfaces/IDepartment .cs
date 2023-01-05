e3e0f48857990e953a9dda4e406a9bbc8a71bfd0
flamur7 <flamur.ga7@gmail.com>
2023-01-05 18:55:53 +0100

    Implementation of interfaces and repositories using the "Unit of Work" design pattern

---
+++ EducationSystem/ES.Application/Services/Interfaces/IDepartment .cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ES.Application.Services.Interfaces
+{
+    public interface IDepartment
+    {
+    }
+}
---
+++ EducationSystem/ES.Application/Services/Interfaces/IGeneric.cs
@@ -0,0 +1,22 @@
+﻿using ES.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ES.Application.Services.Interfaces
+{
+    public interface IGeneric<T> where T : class
+    {
+        Task<IEnumerable<T>> All();
+        Task<T> GetById(int id);
+        Task<bool> Add(T entity);
+        Task<bool> Delete(int id);
+        Task<bool> Upsert(T entity);
+        Task<IEnumerable<T>> Find(Expression<Func<T, bool>> predicate);
+        Task<List<T>> FindList(Expression<Func<T, bool>> predicate);
+    }
+}
